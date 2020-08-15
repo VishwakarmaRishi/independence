6c91ce244f1a0e4f42b5d55c30f0ccfb6d256090
VishwakarmaRishi <68197457+VishwakarmaRishi@users.noreply.github.com>
2020-08-15 09:42:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert ("Happy Independence Day 🇮🇳")
+alert ("Happy Independence Day 🇮🇳");
+alert("But first, tap on the screen!");
+function play() {
+    document.getElementById("sound").play();
+}
+window.addEventListener("mouseover",play);
+window.addEventListener("click",play);
