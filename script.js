bbe44c95801b957de085a78853f32cd2e3cdf318
VishwakarmaRishi <68197457+VishwakarmaRishi@users.noreply.github.com>
2020-08-15 09:47:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert ("Happy Independence Day 🇮🇳");
+alert ("Happy Independence Day \nPround to be an Indian 🇮🇳");
 alert("But first, tap on the screen!");
 function play() {
     document.getElementById("sound").play();
